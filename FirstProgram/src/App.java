be2d3fe9cfa8242c1fbd520397e62f065f54de29
Talha-Ali150 <118039339+Talha-Ali150@users.noreply.github.com>
2023-06-08 03:14:42 +0500

    first commit

---
+++ FirstProgram/src/App.java
@@ -0,0 +1,28 @@
+//without using static method, creating instance of app and then using its function
+// public class App {
+//     public static void main(String[] args) throws Exception {
+//         // System.out.println("Hello, World!");
+
+//         App app = new App();
+//         app.findAvg(10, 20, 30);
+//     }
+
+//     public void findAvg(int a, int b, int c) {
+//         int sum = a + b + c;
+//         int avg = sum / 3;
+//         System.out.println(avg);
+//     }
+// }
+
+// using static method
+public class App {
+    public static void main(String[] args) throws Exception {
+        findAvg(10, 20, 30);
+    }
+
+    public static void findAvg(int a, int b, int c) {
+        int sum = a + b + c;
+        int avg = sum / 3;
+        System.out.println(avg);
+    }
+}
