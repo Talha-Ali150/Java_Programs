549773d2bdd03d70bbc6044cea0f4b734714c724
Talha-Ali150 <syed.talha.ali.150@gmail.com>
2023-06-15 18:11:34 +0500

    program to insert node at beginning of singly linked list

---
+++ FirstProgram/src/App15.java
@@ -0,0 +1,43 @@
+public class App15 {
+    public ListNode head;
+
+    public static class ListNode{
+        public int data;
+        public ListNode next;
+
+        public ListNode(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static void main(String[] args) {
+        App15 sll = new App15();
+
+        sll.head = new ListNode(10);
+        ListNode second = new ListNode(11);
+        ListNode third = new ListNode(12);
+        ListNode fourth = new ListNode(13);
+
+        sll.head.next = second;
+        second.next = third;
+        third.next = fourth;
+
+        sll.display();
+
+        
+        ListNode newNode = new ListNode(9);
+        newNode.next = sll.head;
+        sll.head = newNode;
+        System.out.println();
+
+        sll.display();
+    }
+    
+    public void display(){
+        ListNode current = head;
+        while(current != null){
+            System.out.print(" " + current.data);
+            current = current.next;
+        }
+    }
+}
